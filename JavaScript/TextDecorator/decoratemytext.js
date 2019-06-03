84eab3c4450a44143057f12737f9a13654f9dc39
MauroTravieso <mauro_travieso@hotmail.com>
2019-06-03 17:49:13 -0500

    Update decoratemytext.js

--- JavaScript/TextDecorator/decoratemytext.js
+++ JavaScript/TextDecorator/decoratemytext.js
@@ -1,7 +1,26 @@
 "use strict";
 function displayAlert() {
     //alert("Hello, world!");
-    document.getElementById("textarea").style.fontSize = "24px";
+    //document.getElementById("textarea").style.fontSize = "24px";
+    var elem = document.getElementById("textarea");
+    var theCSSprop = window.getComputedStyle(elem).getPropertyValue("font-size");
+    document.getElementById("textarea").style.fontSize = theCSSprop;
+    console.log(theCSSprop);
+    //theCSSprop = parseInt(theCSSprop + 2);
+    console.log(theCSSprop);
+    document.getElementById("textarea").style.fontSize = theCSSprop+2+'px';
+    //document.getElementById("textarea").style.fontSize = parseInt(theCSSprop+2);
+    console.log(theCSSprop);
 }
 
 document.getElementById('btn').onclick = function() {displayAlert()};
+
+function onChange() {
+    //alert("Hello, world!");
+    document.getElementById("textarea").style.fontWeight = "bold";
+    document.getElementById("textarea").style.color = "green";
+    document.getElementById("textarea").style.textDecorationLine = "underline";
+}
+
+document.getElementById('color').onclick = function() {onChange()};
+
