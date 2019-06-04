c0972fad7c69b50ce150e0bf16af709f86336323
MauroTravieso <mauro_travieso@hotmail.com>
2019-06-04 09:59:07 -0500

    Update ascii.js

--- JavaScript/ASCIIAnimations/ascii.js
+++ JavaScript/ASCIIAnimations/ascii.js
@@ -1,4 +1,5 @@
 "use strict;"
+window.onload = onLoad;
 
 let speed;
 let frames;
@@ -6,8 +7,6 @@ let currFrame;
 let maxFrame;
 let interval;
 let data;
-
-window.onload = onLoad;
 
 function onLoad(){
     setButtonClickEvent("start-button", onclickStart);
@@ -25,8 +24,117 @@ function initVars() {
     speed = 250;
     currFrame = 0;
     maxFrame = -1;
-    frames = null;
+    /*data = ANIMATIONS['Exercise'];
+    console.log(data);
+    frames = data*/
+
+    frames = [];/*null;*/
     interval = null;
     data = null;
+}
+
+function setText(str) {
+    let txtText = getElementById("txtText");
+    txtText.value = str;
+}
+
+function getAnimationFrames() {
+    if (data!=null) {
+        frames = getFrames(data);
+        maxFrame = frames.length;
+        console.log(frames);
+        console.log(currFrame);
+    }
+}
+
+function animations() {
+    setText(frames[currFrame]);
+    currFrame++;
+    if(currFrame===maxFrame)
+        currFrame=0;
+}
+
+function clearTimer() {
+    if(interval!=null) {
+        clearInterval(interval);
+        interval=null;
+    }
+}
+
+function setControlStatus(isInProgress) {
+    disableControlById("stop-button", !isInProgress);
+    disableControlById("start-button", isInProgress);
+    disableControlById("animation",isInProgress);
+    disableControlById("txtText",isInProgress);
+}
+
+function onclickStart() {
+    let txtText = getElementById("txtText");
+    let s = txtText.value;
+    console.log(s);
+
+    getAnimationFrames();
+    clearTimer();
+    currFrame = 0;
+    interval = setInterval(animations,speed);
+    setControlStatus(true);
+}
+
+function onclickStop() {
+    clearTimer();
+    setText(data);
+    setControlStatus(false);
+}
+
+function onChangeAnimation() {
+    let index = this.selectedIndex;
+    let choice = this.options[index].value;
+    data = ANIMATIONS[choice];
+    setText(data);
+}
+
+function onChangeSize() {
+    let index = this.selectedIndex;
+    let choice = this.options[index].value;
+    let txtText = getElementById("txtText");
+    txtText.style.fontSize = choice + "pt";
+}
+
+function setSpeed(s) {
+    speed = s;
+    clearTimer();
+    interval = setInterval(animations,speed);
+}
+
+function onChangeTurbo() {
+    if (this.checked) {
+        setSpeed(50);        
+    } else {
+        setSpeed(250);
+    }
+}
+
+//Event handlers
+function setButtonClickEvent(id, eventHandler) {
+    var btn = getElementById(id);
+    btn.onclick = eventHandler;
+}
+
+function setOnChangeEvent(id, eventHandler) {
+    var element = getElementById(id);
+    element.onchange = eventHandler;
+}
+
+function getElementById(elementId) {
+    return document.getElementById(elementId);
+}
+
+function disableControlById(id, state) {
+    document.getElementById(id).disabled = state;
+    //document.getElementById(id).setAttribute = state;
+}
+
+function getFrames(str) {
+    return str.split("=====\n");
 }
 
