352d44f3d851c733b309423cd0a64f48f4198674
MauroTravieso <mauro_travieso@hotmail.com>
2019-06-06 02:19:31 -0500

    Update ball.js

--- JavaScript/BouncingBall/ball.js
+++ JavaScript/BouncingBall/ball.js
@@ -11,8 +11,10 @@ function update() {
     ballVelocity += 1;
     if (parseInt($("#ball").css('top')) > $(window).height()) {
         ballVelocity *= -.9;
+        
     }
     $("#ball").css('top', function (idx, old) {
         return parseInt(old) + ballVelocity + 'px';
     });
+    console.log(ballVelocity);
 }
