b5c7b372b4ceba32412529edee9ef9e361acda47
MauroTravieso <mauro_travieso@hotmail.com>
2019-06-13 15:53:10 -0500

    Create AuthenticationFilter.java

---
+++ Servlet/Authentication Servlet/AuthenticationFilter.java
@@ -0,0 +1,38 @@
+﻿import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AuthenticationFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession(false);
+
+        String loginURI = request.getContextPath() + "/authenticate";
+
+        boolean loggedIn = session != null && session.getAttribute("uname") != null;
+
+        boolean loginRequest = request.getRequestURI().equals(loginURI);
+
+        if (loggedIn || loginRequest) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.sendRedirect(loginURI);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
