5a9a883f760c7b76d6c6b5bbbff7bd1e07e791dc
MauroTravieso <mauro_travieso@hotmail.com>
2019-06-17 10:52:28 -0500

    Create UserDao.java

---
+++ Servlet/ShoppingCartServlet/dao/UserDao.java
@@ -0,0 +1,42 @@
+﻿package dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import model.User;
+
+public class UserDAO {
+
+    //Map<Integer, User> usersDb = new HashMap<>();
+    HashMap<String, User> usersDb = new HashMap<>();
+//    public void addUser(User user){
+//        usersDb.put(user.getId(),user);
+//    }
+
+    public void addUser(User user){
+        usersDb.put(user.getUserName(),user);
+    }
+
+    public void deleteUser(int id) {
+        usersDb.remove(id);
+    }
+
+//    public void updateUser(User user) {
+//        usersDb.put(user.getId(),user);
+//    }
+
+    public List<User> getAllUsers() {
+        return new ArrayList<>(usersDb.values());
+    }
+
+//    public User getUserById(int id) {
+//        return usersDb.get(id);
+//    }
+
+    public int getSize() {
+        return usersDb.size()+1;
+    }
+
+
+}
