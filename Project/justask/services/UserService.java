862e8b5941e16145566ec06b2e0cbe3a8636d1be
MauroTravieso <mauro_travieso@hotmail.com>
2019-06-18 01:32:41 -0500

    Create UserService.java

---
+++ Project/justask/services/UserService.java
@@ -0,0 +1,18 @@
+﻿package services;
+
+import controllers.DB;
+
+
+import java.sql.Connection;
+
+public class UserService {
+    Connection connection;
+
+    public  UserService(){
+        connection = DB.getConnection();
+    }
+
+
+
+
+}
