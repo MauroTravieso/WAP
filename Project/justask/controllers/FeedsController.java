53108f3e2b409385c0f3937487e68b6462ae78e1
MauroTravieso <mauro_travieso@hotmail.com>
2019-06-18 01:33:45 -0500

    Create FeedsController.java

---
+++ Project/justask/controllers/FeedsController.java
@@ -0,0 +1,64 @@
+﻿package controllers;
+
+import entity.Tag;
+import services.FeedService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/feed")
+public class FeedsController extends HttpServlet {
+    private FeedService service;
+
+    @Override
+    public void init() throws ServletException {
+        service = new FeedService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        //Mauro
+        HttpSession session = req.getSession();
+        Integer username = (Integer)session.getAttribute("userId");
+        System.out.println("USERNAME:" + username);
+        //resp.sendRedirect(req.getContextPath() + "/profile");
+        //Mauro
+
+        try {
+            req.setAttribute("tags", service.getAllTags());
+
+            String tag = req.getParameter("tag");
+            if(tag != null){
+                req.setAttribute("feeds", service.getFeedsByTag(tag));
+            }else{
+                req.setAttribute("feeds", service.getAllFeeds());
+            }
+
+
+
+
+        } catch (Exception e) {
+            e.getMessage();
+            req.setAttribute("tags", new ArrayList<Tag>());
+        }
+
+        req.getRequestDispatcher("feeds.jsp").forward(req, resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
