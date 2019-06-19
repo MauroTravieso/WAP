ad3d73b5305e0834d837ece4d59f7fb17cca366e
MauroTravieso <mauro_travieso@hotmail.com>
2019-06-19 15:44:41 -0500

    Create Answer.java

---
+++ WAPProject/src/main/java/entity/Answer.java
@@ -0,0 +1,79 @@
+package entity;
+
+public class Answer {
+    private Integer id;
+    private Integer questionId;
+    private String content;
+    private String createdAt;
+    private String updatedAt;
+    private Integer userId;
+    private String answerUser;
+    private Integer upVotes;
+
+    public Answer(){
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Integer questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getAnswerUser() {
+        return answerUser;
+    }
+
+    public void setAnswerUser(String answerUser) {
+        this.answerUser = answerUser;
+    }
+
+    public Integer getUpVotes() {
+        return upVotes;
+    }
+
+    public void setUpVotes(Integer upVotes) {
+        this.upVotes = upVotes;
+    }
+}
