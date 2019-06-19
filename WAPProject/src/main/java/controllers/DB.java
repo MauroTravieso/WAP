64b2867abf45f51116e2ee88e9c94945f3b2a7c2
MauroTravieso <mauro_travieso@hotmail.com>
2019-06-19 15:45:18 -0500

    Create DB.java

---
+++ WAPProject/src/main/java/controllers/DB.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+
+public class DB {
+    protected static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            if (connection != null) {
+                return connection;
+            }
+            MysqlDataSource source = new MysqlDataSource();
+            source.setUser("root");
+            source.setPassword("omni0818");
+            source.setDatabaseName("justask");
+            source.setPort(3306);
+            source.setServerName("localhost");
+
+            connection = source.getConnection();
+            return connection;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
