00eda4a2376b83418876093ceb269924c1559ce2
MauroTravieso <mauro_travieso@hotmail.com>
2019-06-19 15:44:34 -0500

    Create Feed.java

---
+++ WAPProject/src/main/java/entity/Feed.java
@@ -0,0 +1,118 @@
+package entity;
+
+public class Feed {
+    private String title;
+    private Integer id;
+    private Integer upVotes;
+    private Integer downVotes;
+    private Integer userId;
+    private String answerUser;
+    private String questionUser;
+    private String answer;
+    private String tags;
+    private String createdAt;
+    private String updatedAt;
+
+    public Integer getUpVotes() {
+        return upVotes;
+    }
+
+    public void setUpVotes(Integer upVotes) {
+        this.upVotes = upVotes;
+    }
+
+    public Integer getDownVotes() {
+        return downVotes;
+    }
+
+    public void setDownVotes(Integer downVotes) {
+        this.downVotes = downVotes;
+    }
+
+
+    public Feed() {
+        title = "";
+        id = null;
+    }
+
+    public Feed(Integer id, String title, Integer userId, String createdAt, String updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.userId = userId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getAnswerUser() {
+        return answerUser;
+    }
+
+    public void setAnswerUser(String answerUser) {
+        this.answerUser = answerUser;
+    }
+
+    public String getQuestionUser() {
+        return questionUser;
+    }
+
+    public void setQuestionUser(String questionUser) {
+        this.questionUser = questionUser;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+}
+
